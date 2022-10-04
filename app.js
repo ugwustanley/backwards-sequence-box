3e1d6f634cf1e716161ddaad74529404d11ba25f
Ugwu Stanley <ugwuchiagoziestanley@gmail.com>
2022-10-04 15:57:13 +0100

    added reverse

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ for (let i = 0; i < 6; i++) {
   boxes[i].addEventListener("click", function (e) {
     //check if item already exists
     const itemExists = active_boxes.find((box) => box.boxIndex === i);
-    if (itemExists) return;
+    if (itemExists && active_boxes.length === 6) return;
     //check if active_boxes is not at maximum
     if (active_boxes.length < 5) {
       e.target.style.backgroundColor = "teal";
@@ -22,9 +22,19 @@ for (let i = 0; i < 6; i++) {
       boxIndex: i,
       background: "transparent",
     });
+    //reverse active_boxes
+    const reversed_active_boxes = []
+      .concat(active_boxes)
+      .sort((a, b) =>
+        active_boxes.indexOf(a.boxIndex) < active_boxes.indexOf(b.boxIndex)
+          ? 1
+          : -1
+      );
+
+    console.log(reversed_active_boxes, active_boxes);
 
     //remove background color using the click sequence
-    active_boxes.forEach((box, index) => {
+    reversed_active_boxes.forEach((box, index) => {
       setTimeout(() => {
         boxes[box.boxIndex].style.backgroundColor = box.background;
       }, 400 * (index + 1));
